5eb545409b06368a321c2fdd23a32e2045fb895f
ksibdesign <ksibley@mywebgrocer.com>
2015-02-16 14:57:58 -0500

    Base Planechase Gen
    These are the base/original Planechase Gen files

---
+++ decks.js
@@ -0,0 +1,714 @@
+function deckAll() {
+var temp2 = 0;
+deckName = "All";
+
+deckCurrent[temp2] = "Academy at Tolaria West";
+temp2++;
+
+deckCurrent[temp2] = "The Æther Flues";
+temp2++;
+
+deckCurrent[temp2] = "Agyrem";
+temp2++;
+
+deckCurrent[temp2] = "Bant";
+temp2++;
+
+deckCurrent[temp2] = "Cliffside Market";
+temp2++;
+
+deckCurrent[temp2] = "The Dark Barony";
+temp2++;
+
+deckCurrent[temp2] = "Eloren Wilds";
+temp2++;
+
+deckCurrent[temp2] = "The Eon Fog";
+temp2++;
+
+deckCurrent[temp2] = "Feeding Grounds";
+temp2++;
+
+deckCurrent[temp2] = "Fields of Summer";
+temp2++;
+
+deckCurrent[temp2] = "The Fourth Sphere";
+temp2++;
+
+deckCurrent[temp2] = "Glimmervoid Basin";
+temp2++;
+
+deckCurrent[temp2] = "Goldmeadow";
+temp2++;
+
+deckCurrent[temp2] = "The Great Forest";
+temp2++;
+
+deckCurrent[temp2] = "Grixis";
+temp2++;
+
+deckCurrent[temp2] = "The Hippodrome";
+temp2++;
+
+deckCurrent[temp2] = "Immersturm";
+temp2++;
+
+deckCurrent[temp2] = "Isle of Vesuva";
+temp2++;
+
+deckCurrent[temp2] = "Izzet Steam Maze";
+temp2++;
+
+deckCurrent[temp2] = "Krosa";
+temp2++;
+
+deckCurrent[temp2] = "Lethe Lake";
+temp2++;
+
+deckCurrent[temp2] = "Llanowar";
+temp2++;
+
+deckCurrent[temp2] = "The Maelstrom";
+temp2++;
+
+deckCurrent[temp2] = "Minamo";
+temp2++;
+
+deckCurrent[temp2] = "Murasa";
+temp2++;
+
+deckCurrent[temp2] = "Naar Isle";
+temp2++;
+
+deckCurrent[temp2] = "Naya";
+temp2++;
+
+deckCurrent[temp2] = "Otaria";
+temp2++;
+
+deckCurrent[temp2] = "Panopticon";
+temp2++;
+
+deckCurrent[temp2] = "Pools of Becoming";
+temp2++;
+
+deckCurrent[temp2] = "Raven's Run";
+temp2++;
+
+deckCurrent[temp2] = "Sanctum of Serra";
+temp2++;
+
+deckCurrent[temp2] = "Sea of Sand";
+temp2++;
+
+deckCurrent[temp2] = "Shiv";
+temp2++;
+
+deckCurrent[temp2] = "Skybreen";
+temp2++;
+
+deckCurrent[temp2] = "Sokenzan";
+temp2++;
+
+deckCurrent[temp2] = "Stronghold Furnace";
+temp2++;
+
+deckCurrent[temp2] = "Turri Island";
+temp2++;
+
+deckCurrent[temp2] = "Undercity Reaches";
+temp2++;
+
+deckCurrent[temp2] = "Velis Vel";
+temp2++;
+
+deckCurrent[temp2] = "Tazeem";
+temp2++;
+
+deckCurrent[temp2] = "Celestine Reef";
+temp2++;
+
+deckCurrent[temp2] = "Horizon Boughs";
+temp2++;
+
+deckCurrent[temp2] = "Mirrored Depths";
+temp2++;
+
+deckCurrent[temp2] = "Tember City";
+temp2++;
+
+// Planechase 2012
+
+deckCurrent[temp2] = "Chaotic Æther";
+temp2++;
+
+deckCurrent[temp2] = "Interplanar Tunnel";
+temp2++;
+
+deckCurrent[temp2] = "Morphic Tide";
+temp2++;
+
+deckCurrent[temp2] = "Mutual Epiphany";
+temp2++;
+
+deckCurrent[temp2] = "Planewide Disaster";
+temp2++;
+
+deckCurrent[temp2] = "Reality Shaping";
+temp2++;
+
+deckCurrent[temp2] = "Spatial Merging";
+temp2++;
+
+deckCurrent[temp2] = "Time Distortion";
+temp2++;
+
+deckCurrent[temp2] = "Akoum";
+temp2++;
+
+deckCurrent[temp2] = "Aretopolis";
+temp2++;
+
+deckCurrent[temp2] = "Astral Arena";
+temp2++;
+
+deckCurrent[temp2] = "Bloodhill Bastion";
+temp2++;
+
+deckCurrent[temp2] = "Edge of Malacol";
+temp2++;
+
+deckCurrent[temp2] = "Furnace Layer";
+temp2++;
+
+deckCurrent[temp2] = "Gavony";
+temp2++;
+
+deckCurrent[temp2] = "Glen Elendra";
+temp2++;
+
+deckCurrent[temp2] = "Grand Ossuary";
+temp2++;
+
+deckCurrent[temp2] = "Grove of the Dreampods";
+temp2++;
+
+deckCurrent[temp2] = "Hedron Fields of Agadeem";
+temp2++;
+
+deckCurrent[temp2] = "Jund";
+temp2++;
+
+deckCurrent[temp2] = "Kessig";
+temp2++;
+
+deckCurrent[temp2] = "Kharasha Foothills";
+temp2++;
+
+deckCurrent[temp2] = "Kilnspire District";
+temp2++;
+
+deckCurrent[temp2] = "Lair of the Ashen Idol";
+temp2++;
+
+deckCurrent[temp2] = "Mount Keralia";
+temp2++;
+
+deckCurrent[temp2] = "Nephalia";
+temp2++;
+
+deckCurrent[temp2] = "Norn's Dominion";
+temp2++;
+
+deckCurrent[temp2] = "Onakke Catacomb";
+temp2++;
+
+deckCurrent[temp2] = "Orochi Colony";
+temp2++;
+
+deckCurrent[temp2] = "Orzhova";
+temp2++;
+
+deckCurrent[temp2] = "Prahv";
+temp2++;
+
+deckCurrent[temp2] = "Quicksilver Sea";
+temp2++;
+
+deckCurrent[temp2] = "Selesnya Loft Gardens";
+temp2++;
+
+deckCurrent[temp2] = "Stensia";
+temp2++;
+
+deckCurrent[temp2] = "Takenuma";
+temp2++;
+
+deckCurrent[temp2] = "Talon Gates";
+temp2++;
+
+deckCurrent[temp2] = "Trail of the Mage-Rings";
+temp2++;
+
+deckCurrent[temp2] = "Truga Jungle";
+temp2++;
+
+deckCurrent[temp2] = "Windriddle Palaces";
+temp2++;
+
+deckCurrent[temp2] = "The Zephyr Maze";
+temp2++;
+
+deckCurrent[temp2] = "Stairs to Infinity";
+temp2++;
+
+//alert(deckCurrent.toString());
+//alert(deckCurrent.length);
+}
+function deckElementalThunder() {
+var temp2 = 0;
+deckName = "Elemental Thunder";
+
+deckCurrent[temp2] = "Eloren Wilds";
+temp2++;
+
+deckCurrent[temp2] = "Feeding Grounds";
+temp2++;
+
+deckCurrent[temp2] = "The Great Forest";
+temp2++;
+
+deckCurrent[temp2] = "Immersturm";
+temp2++;
+
+deckCurrent[temp2] = "Isle of Vesuva";
+temp2++;
+
+deckCurrent[temp2] = "Llanowar";
+temp2++;
+
+deckCurrent[temp2] = "Murasa";
+temp2++;
+
+deckCurrent[temp2] = "Naya";
+temp2++;
+
+deckCurrent[temp2] = "Turri Island";
+temp2++;
+
+deckCurrent[temp2] = "Velis Vel";
+temp2++;
+}
+function deckMetallicDreams() {
+var temp2 = 0;
+deckName = "Metallic Dreams";
+
+deckCurrent[temp2] = "Bant";
+temp2++;
+
+deckCurrent[temp2] = "The Eon Fog";
+temp2++;
+
+deckCurrent[temp2] = "Goldmeadow";
+temp2++;
+
+deckCurrent[temp2] = "The Hippodrome";
+temp2++;
+
+deckCurrent[temp2] = "Krosa";
+temp2++;
+
+deckCurrent[temp2] = "The Maelstrom";
+temp2++;
+
+deckCurrent[temp2] = "Minamo";
+temp2++;
+
+deckCurrent[temp2] = "Panopticon";
+temp2++;
+
+deckCurrent[temp2] = "Sanctum of Serra";
+temp2++;
+
+deckCurrent[temp2] = "Undercity Reaches";
+temp2++;
+}
+function deckStrikeForce() {
+var temp2 = 0;
+deckName = "Strike Force";
+
+deckCurrent[temp2] = "Academy at Tolaria West";
+temp2++;
+
+deckCurrent[temp2] = "Agyrem";
+temp2++;
+
+deckCurrent[temp2] = "Glimmervoid Basin";
+temp2++;
+
+deckCurrent[temp2] = "Izzet Steam Maze";
+temp2++;
+
+deckCurrent[temp2] = "Naar Isle";
+temp2++;
+
+deckCurrent[temp2] = "Otaria";
+temp2++;
+
+deckCurrent[temp2] = "Shiv";
+temp2++;
+
+deckCurrent[temp2] = "Skybreen";
+temp2++;
+
+deckCurrent[temp2] = "Sokenzan";
+temp2++;
+
+deckCurrent[temp2] = "Stronghold Furnace";
+temp2++;
+}
+function deckZombieEmpire() {
+var temp2 = 0;
+deckName = "Zombie Empire";
+
+deckCurrent[temp2] = "The Æther Flues";
+temp2++;
+
+deckCurrent[temp2] = "Cliffside Market";
+temp2++;
+
+deckCurrent[temp2] = "The Dark Barony";
+temp2++;
+
+deckCurrent[temp2] = "Fields of Summer";
+temp2++;
+
+deckCurrent[temp2] = "The Fourth Sphere";
+temp2++;
+
+deckCurrent[temp2] = "Grixis";
+temp2++;
+
+deckCurrent[temp2] = "Lethe Lake";
+temp2++;
+
+deckCurrent[temp2] = "Pools of Becoming";
+temp2++;
+
+deckCurrent[temp2] = "Raven's Run";
+temp2++;
+
+deckCurrent[temp2] = "Sea of Sand";
+temp2++;
+}
+function deckChaosReigns() {
+var temp2 = 0;
+deckName = "Chaos Reigns";
+
+deckCurrent[temp2] = "Grand Ossuary";
+temp2++;
+
+deckCurrent[temp2] = "Hedron Fields of Agadeem";
+temp2++;
+
+deckCurrent[temp2] = "Mount Keralia";
+temp2++;
+
+deckCurrent[temp2] = "Nephalia";
+temp2++;
+
+deckCurrent[temp2] = "Orzhova";
+temp2++;
+
+deckCurrent[temp2] = "Trail of the Mage-Rings";
+temp2++;
+
+deckCurrent[temp2] = "Truga Jungle";
+temp2++;
+
+deckCurrent[temp2] = "Windriddle Palaces";
+temp2++;
+
+deckCurrent[temp2] = "Mutual Epiphany";
+temp2++;
+
+deckCurrent[temp2] = "Time Distortion";
+temp2++;
+}
+function deckNightOfTheNinja() {
+var temp2 = 0;
+deckName = "Night of the Ninja";
+
+deckCurrent[temp2] = "Bloodhill Bastion";
+temp2++;
+
+deckCurrent[temp2] = "Gavony";
+temp2++;
+
+deckCurrent[temp2] = "Glen Elendra";
+temp2++;
+
+deckCurrent[temp2] = "Kharasha Foothills";
+temp2++;
+
+deckCurrent[temp2] = "Norn's Dominion";
+temp2++;
+
+deckCurrent[temp2] = "Quicksilver Sea";
+temp2++;
+
+deckCurrent[temp2] = "Takenuma";
+temp2++;
+
+deckCurrent[temp2] = "The Zephyr Maze";
+temp2++;
+
+deckCurrent[temp2] = "Interplanar Tunnel";
+temp2++;
+
+deckCurrent[temp2] = "Morphic Tide";
+temp2++;
+}
+function deckPrimordialHunger() {
+var temp2 = 0;
+deckName = "Primordial Hunger";
+
+deckCurrent[temp2] = "Furnace Layer";
+temp2++;
+
+deckCurrent[temp2] = "Jund";
+temp2++;
+
+deckCurrent[temp2] = "Kilnspire District";
+temp2++;
+
+deckCurrent[temp2] = "Lair of the Ashen Idol";
+temp2++;
+
+deckCurrent[temp2] = "Orochi Colony";
+temp2++;
+
+deckCurrent[temp2] = "Prahv";
+temp2++;
+
+deckCurrent[temp2] = "Selesnya Loft Garden";
+temp2++;
+
+deckCurrent[temp2] = "Stensia";
+temp2++;
+
+deckCurrent[temp2] = "Reality Shaping";
+temp2++;
+
+deckCurrent[temp2] = "Spatial Merging";
+temp2++;
+}
+function deckSavageAuras() {
+var temp2 = 0;
+deckName = "Savage Auras";
+
+deckCurrent[temp2] = "Akoum";
+temp2++;
+
+deckCurrent[temp2] = "Aretopolis";
+temp2++;
+
+deckCurrent[temp2] = "Astral Arena";
+temp2++;
+
+deckCurrent[temp2] = "Edge of Malacol";
+temp2++;
+
+deckCurrent[temp2] = "Grove of the Dreampods";
+temp2++;
+
+deckCurrent[temp2] = "Kessig";
+temp2++;
+
+deckCurrent[temp2] = "Onakke Catacomb";
+temp2++;
+
+deckCurrent[temp2] = "Talon Gates";
+temp2++;
+
+deckCurrent[temp2] = "Chaotic Æther";
+temp2++;
+
+deckCurrent[temp2] = "Planewide Disaster";
+temp2++;
+}
+function deckCustomPlaceholder() {
+var temp2 = 0;
+deckPlaceholder[temp2] = "Academy at Tolaria West";
+temp2++;
+deckPlaceholder[temp2] = "Agyrem";
+temp2++;
+deckPlaceholder[temp2] = "Akoum";
+temp2++;
+deckPlaceholder[temp2] = "Aretopolis";
+temp2++;
+deckPlaceholder[temp2] = "Astral Arena";
+temp2++;
+deckPlaceholder[temp2] = "Bant";
+temp2++;
+deckPlaceholder[temp2] = "Bloodhill Bastion";
+temp2++;
+deckPlaceholder[temp2] = "Celestine Reef";
+temp2++;
+deckPlaceholder[temp2] = "Chaotic Æther";
+temp2++;
+deckPlaceholder[temp2] = "Cliffside Market";
+temp2++;
+deckPlaceholder[temp2] = "Edge of Malacol";
+temp2++;
+deckPlaceholder[temp2] = "Eloren Wilds";
+temp2++;
+deckPlaceholder[temp2] = "Feeding Grounds";
+temp2++;
+deckPlaceholder[temp2] = "Fields of Summer";
+temp2++;
+deckPlaceholder[temp2] = "Furnace Layer";
+temp2++;
+deckPlaceholder[temp2] = "Gavony";
+temp2++;
+deckPlaceholder[temp2] = "Glen Elendra";
+temp2++;
+deckPlaceholder[temp2] = "Glimmervoid Basin";
+temp2++;
+deckPlaceholder[temp2] = "Goldmeadow";
+temp2++;
+deckPlaceholder[temp2] = "Grand Ossuary";
+temp2++;
+deckPlaceholder[temp2] = "Grixis";
+temp2++;
+deckPlaceholder[temp2] = "Grove of the Dreampods";
+temp2++;
+deckPlaceholder[temp2] = "Hedron Fields of Agadeem";
+temp2++;
+deckPlaceholder[temp2] = "Horizon Boughs";
+temp2++;
+deckPlaceholder[temp2] = "Immersturm";
+temp2++;
+deckPlaceholder[temp2] = "Interplanar Tunnel";
+temp2++;
+deckPlaceholder[temp2] = "Isle of Vesuva";
+temp2++;
+deckPlaceholder[temp2] = "Izzet Steam Maze";
+temp2++;
+deckPlaceholder[temp2] = "Jund";
+temp2++;
+deckPlaceholder[temp2] = "Kessig";
+temp2++;
+deckPlaceholder[temp2] = "Kharasha Foothills";
+temp2++;
+deckPlaceholder[temp2] = "Kilnspire District";
+temp2++;
+deckPlaceholder[temp2] = "Krosa";
+temp2++;
+deckPlaceholder[temp2] = "Lair of the Ashen Idol";
+temp2++;
+deckPlaceholder[temp2] = "Lethe Lake";
+temp2++;
+deckPlaceholder[temp2] = "Llanowar";
+temp2++;
+deckPlaceholder[temp2] = "Minamo";
+temp2++;
+deckPlaceholder[temp2] = "Mirrored Depths";
+temp2++;
+deckPlaceholder[temp2] = "Morphic Tide";
+temp2++;
+deckPlaceholder[temp2] = "Mount Keralia";
+temp2++;
+deckPlaceholder[temp2] = "Murasa";
+temp2++;
+deckPlaceholder[temp2] = "Mutual Epiphany";
+temp2++;
+deckPlaceholder[temp2] = "Naar Isle";
+temp2++;
+deckPlaceholder[temp2] = "Naya";
+temp2++;
+deckPlaceholder[temp2] = "Nephalia";
+temp2++;
+deckPlaceholder[temp2] = "Norn's Dominion";
+temp2++;
+deckPlaceholder[temp2] = "Onakke Catacomb";
+temp2++;
+deckPlaceholder[temp2] = "Orochi Colony";
+temp2++;
+deckPlaceholder[temp2] = "Orzhova";
+temp2++;
+deckPlaceholder[temp2] = "Otaria";
+temp2++;
+deckPlaceholder[temp2] = "Panopticon";
+temp2++;
+deckPlaceholder[temp2] = "Planewide Disaster";
+temp2++;
+deckPlaceholder[temp2] = "Pools of Becoming";
+temp2++;
+deckPlaceholder[temp2] = "Prahv";
+temp2++;
+deckPlaceholder[temp2] = "Quicksilver Sea";
+temp2++;
+deckPlaceholder[temp2] = "Raven's Run";
+temp2++;
+deckPlaceholder[temp2] = "Reality Shaping";
+temp2++;
+deckPlaceholder[temp2] = "Sanctum of Serra";
+temp2++;
+deckPlaceholder[temp2] = "Sea of Sand";
+temp2++;
+deckPlaceholder[temp2] = "Selesnya Loft Gardens";
+temp2++;
+deckPlaceholder[temp2] = "Shiv";
+temp2++;
+deckPlaceholder[temp2] = "Skybreen";
+temp2++;
+deckPlaceholder[temp2] = "Sokenzan";
+temp2++;
+deckPlaceholder[temp2] = "Spatial Merging";
+temp2++;
+deckPlaceholder[temp2] = "Stairs to Infinity";
+temp2++;
+deckPlaceholder[temp2] = "Stensia";
+temp2++;
+deckPlaceholder[temp2] = "Stronghold Furnace";
+temp2++;
+deckPlaceholder[temp2] = "Takenuma";
+temp2++;
+deckPlaceholder[temp2] = "Talon Gates";
+temp2++;
+deckPlaceholder[temp2] = "Tazeem";
+temp2++;
+deckPlaceholder[temp2] = "Tember City";
+temp2++;
+deckPlaceholder[temp2] = "The Æther Flues";
+temp2++;
+deckPlaceholder[temp2] = "The Dark Barony";
+temp2++;
+deckPlaceholder[temp2] = "The Eon Fog";
+temp2++;
+deckPlaceholder[temp2] = "The Fourth Sphere";
+temp2++;
+deckPlaceholder[temp2] = "The Great Forest";
+temp2++;
+deckPlaceholder[temp2] = "The Hippodrome";
+temp2++;
+deckPlaceholder[temp2] = "The Maelstrom";
+temp2++;
+deckPlaceholder[temp2] = "The Zephyr Maze";
+temp2++;
+deckPlaceholder[temp2] = "Time Distortion";
+temp2++;
+deckPlaceholder[temp2] = "Trail of the Mage-Rings";
+temp2++;
+deckPlaceholder[temp2] = "Truga Jungle";
+temp2++;
+deckPlaceholder[temp2] = "Turri Island";
+temp2++;
+deckPlaceholder[temp2] = "Undercity Reaches";
+temp2++;
+deckPlaceholder[temp2] = "Velis Vel";
+temp2++;
+deckPlaceholder[temp2] = "Windriddle Palaces";
+temp2++;
+}
